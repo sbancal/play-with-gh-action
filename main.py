7c3ed5fcc43085b8a2f227bc024d4913bbe95e70
Samuel Bancal <Samuel.Bancal@epfl.ch>
2023-05-10 16:13:07 +0200

    feat: add ENAC_IT4R_CD_environment

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog="main", description="just a test")
+parser.add_argument("--env", type=str, help="environment")
+args = parser.parse_args()
+
 print("hello world from main.py : )")
+print("env: ", args.env)
